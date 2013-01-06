3ba4c85fe492a3202c9c7d67e3421bfa7b097338
bjornd <echo.bjornd@gmail.com>
2013-01-06 20:59:55 +0200

    Fixed production script

--- production.sh
+++ production.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-gacode=`cat gacode`
-sed '/%gacode%/r gacode' <index-dev.html >index-prod.html
-sed -i -e 's/%gacode%//g' index-prod.html
+sed '/%gacode%/{
+    s/%gacode%//g
+    r gacode
+}' <index-dev.html >index-prod.html
